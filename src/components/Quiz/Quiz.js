90bf3e74562f4bb10db0f0bf5ad388e597b02156
Khairul38 <khairulislam38@gmail.com>
2021-12-25 23:21:42 +0600

    update

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Question from '../Question/Question';
 import QuestionCounter from '../QuestionCounter/QuestionCounter';
+import './Quiz.css'
 
 const Quiz = () => {
     return (
         <div className='m-5'>
             <h2 className='text-center'>Quiz Title</h2>
-            <div className='row mt-5 g-0'>
+            <div className="timer mt-5">
+                <div className="time_left_txt">Time Left</div>
+                <div className="timer_sec">15</div>
+            </div>
+            <div className='row mt-3 g-0'>
                 <div className='col-lg-8'>
                     <Question></Question>
                 </div>
